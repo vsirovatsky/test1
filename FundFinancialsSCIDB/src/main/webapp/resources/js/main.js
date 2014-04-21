a26b2c5eda767ce55b4f5d7f32c54ed7768d0d9b
Alex <asyrovatsky@gmail.com>
2014-04-21 18:58:41 +0300

    Filled modal window with details view

--- FundFinancialsSCIDB/src/main/webapp/resources/js/main.js
+++ FundFinancialsSCIDB/src/main/webapp/resources/js/main.js
@@ -8,8 +8,9 @@
 			Urls: {},
 			Data: {},
 			TempData: {},
+			DetailData: {data: [{id:1}, {id: 2}]},
 			EventHandler: {},
-			currentTableView: {},
+			currentRouteLink: null,
 			initData: function(rest_url){
 				$.ajax({
 			         url: App.Urls.baseUrl+ '/api/' + rest_url,
@@ -46,8 +47,7 @@
 				var router = new App.Routers.MainRouter();				
 				Backbone.history.start();
 				
-				App.initData('financials/full');
-				
+				App.initData('financials/full');				
 				$("#tabDiv").empty().append(new App.Views.MainView().render().el);
 				new App.Views.TableView().render().swapView(null);
 				router.navigate("show_balance", {trigger: true});
@@ -70,9 +70,9 @@
 					if (data.from == "") data.from = "blank";
 					if (data.till == "") data.till = "blank";
 					var str = 'financials/dates/' + data.from + '/' + data.till;					
-					App.initData(str);
-					App.currentTableView.rerender();
-					//router.navigate("main", {trigger: true});
+					App.initData(str);				
+					router.navigate("/", {trigger: false});
+					router.navigate(App.currentRouteLink, {trigger: true});
 				});
 				
 			}		
@@ -99,6 +99,25 @@
 		
 	});
 	
+	App.Views.ItemsDetailTableView = Backbone.View.extend({
+		tagName: 'div',		   	
+		render: function(){
+			var innerHtml = '<table class="modal-table">';
+			
+			if (App.DetailData != null){
+				for (var i = 0; i < App.DetailData.items.length; i++){
+					innerHtml += '<tr><td>' + JSON.stringify(App.DetailData.items[i]) + '</td></tr>'; 
+				}
+			}
+			innerHtml += '</table>';
+			this.$el.html(innerHtml);			
+			
+			return this;
+		}
+		
+	});
+		
+	
 	App.Views.ItemsDetailView = Backbone.ModalView.extend(
 			{
 			    name: "Modal Window",
@@ -109,19 +128,90 @@
 			        function()
 			        {
 			            _.bindAll( this, "render");
-			            //this.template = _.template( this.templateHtml);
-			            this.template = "<h1>";
-			            this.template += this.model.get('chart');
-			            this.template += "</h1>";
-			        },
-			    events:
-			        {
 			            
-			        },			    
+			            this.initData(1);
+			            
+			            //this.template = _.template( this.templateHtml);
+			            
+			            
+			        },
+			    events: {
+			    		"click .show" : "show",	
+					},	
+				"show": function(){
+					this.initData(1);
+					this.swapViews();					
+				},	
 			    render: function(){
-			            $(this.el).html( this.template);
-			            return this;
-			        }
+			    	
+			    	
+			    	if (this.currentTableView != null) this.currentTableView.remove();		            
+			    	this.currentTableView = new App.Views.ItemsDetailTableView();
+			    	
+			    	this.template = "<h1>";
+		            this.template += this.model.get('chart');
+		            this.template += "</h1>";
+		            this.template += '<div id="modalTableContainer"> </div>';	            
+		            
+		            this.template += '<INPUT TYPE="button" value="Show" class="show" id="modal_button"/>';
+			        $(this.el).html( this.template);
+			        this.currentTableView.setElement(this.$('#modalTableContainer')).render();
+			        return this;
+			    },
+			    currentTableView: null,  
+			    swapViews: function(){
+			    	if (this.currentTableView != null) this.currentTableView.remove();		            
+			    	this.currentTableView = new App.Views.ItemsDetailTableView();
+			    	
+			    	this.currentTableView.setElement(this.$('#modalTableContainer')).render();
+		            
+			    },			    
+			    getInfo: function(){
+			    	var from_str = $('#datepicker_from').val();
+					var till_str = $('#datepicker_till').val();
+			    	if (from_str == "") from_str = "blank";
+					if (till_str == "") till_str = "blank";
+					
+			    	if (App.currentRouteLink == "show_balance"){
+			    		return {
+			    			tabIndex: 1,
+			    			from: from_str,
+			    			till: till_str
+			    		};
+			    	} else if (App.currentRouteLink == "show_statements"){
+			    		return {
+			    			tabIndex: 2,
+			    			from: from_str,
+			    			till: till_str
+			    		};
+			    	} else if (App.currentRouteLink == "show_cash_flow"){
+			    		return {
+			    			tabIndex: 3,
+			    			from: from_str,
+			    			till: till_str
+			    		};
+			    	} else {
+			    		return {
+			    			tabIndex: 4,
+			    			from: from_str,
+			    			till: till_str
+			    		};
+			    	}
+			    },    
+			    initData: function(page){
+			    	var infoObj = this.getInfo();
+			    	var rest_url = 'financials/details/' + infoObj.tabIndex + '/' + this.model.get('chart') + '/' + infoObj.from + '/' + infoObj.till + '/' + page;
+			    	$.ajax({
+				         url: App.Urls.baseUrl+ '/api/' + rest_url,
+				         success: function(data) {				        	 	
+				        	alert(this.url);	
+							alert(JSON.stringify(data));
+							App.DetailData = data;
+								
+			             },
+			             async:   false
+					});
+			    }    
 			});
 	
 	App.Views.ControlPanel = Backbone.View.extend({
@@ -459,34 +549,38 @@
 			"show_statements"      : "show_statements"
 		},
 		main: function(){
-			
+			App.currentRouteLink = "main";
 			$("#tabDiv").empty().append(new App.Views.MainView().render().el);
 			new App.Views.TableView().render().swapView(null);
 			
 		},
 		show_balance: function(){
+			App.currentRouteLink = "show_balance";
 			$('.tabs .tab-links .balance span').parent('li').addClass('active').siblings().removeClass('active');	 
 			var col = new App.Collections.BalanceCollection(App.TempData.balanceModelsArray);
-			var v1 = new App.Views.BalanceCollectionView({collection: col});
-			App.currentTableView = new App.Views.TableView().render().swapView(v1);
+			var v1 = new App.Views.BalanceCollectionView({collection: col});			
+			new App.Views.TableView().render().swapView(v1);
 		},
 		show_cash_flow: function(){		
+			App.currentRouteLink = "show_cash_flow";
 			$('.tabs .tab-links .cash_flow span').parent('li').addClass('active').siblings().removeClass('active');
 			var col = new App.Collections.CashFlowCollection(App.TempData.cashFlowModelsArray);
 			var v1 = new App.Views.CashFlowCollectionView({collection: col});
-			App.currentTableView = new App.Views.TableView().render().swapView(v1);
+			new App.Views.TableView().render().swapView(v1);
 		},
 		show_partner_capital: function(){
+			App.currentRouteLink = "show_partner_capital";
 			$('.tabs .tab-links .partner_capital span').parent('li').addClass('active').siblings().removeClass('active');
 			var col = new App.Collections.PartnerCapitalCollection(App.TempData.partnerCapitalModelsArray);
 			var v1 = new App.Views.PartnerCapitalCollectionView({collection: col});
-			App.currentTableView = new App.Views.TableView().render().swapView(v1);
+			new App.Views.TableView().render().swapView(v1);
 		},
 		show_statements: function(){
+			App.currentRouteLink = "show_statements";
 			$('.tabs .tab-links .statements span').parent('li').addClass('active').siblings().removeClass('active');
 			var col = new App.Collections.StatementsCollection(App.TempData.statementsModelsArray);
 			var v1 = new App.Views.StatementsCollectionView({collection: col});
-			App.currentTableView = new App.Views.TableView().render().swapView(v1);	
+			new App.Views.TableView().render().swapView(v1);	
 			
 			
 			
@@ -497,8 +591,8 @@
 	function showModalView(modelIn){
 		
 		new App.Views.ItemsDetailView({model: modelIn}).render().showModal({
-		    x: $(window).width()/2 - 100,
-		    y: $(window).height()/2 - 100,
+		    x: 200,
+		    y: 200,
 		    fadeInDuration:150,
 		    fadeOutDuration:150,
 		    showCloseButton:true,
@@ -511,7 +605,8 @@
 		    closeImageUrl: "resources/close-modal.png",
 		    closeImageHoverUrl: "resources/close-modal-hover.png",
 		    css:
-		    {
+		    {	
+		    	"width": "800px",		    	
 		        "border": "2px solid #111",
 		        "background-color": "#fff",
 		        "-webkit-box-shadow": "0px 0px 15px 4px rgba(0, 0, 0, 0.5)",
